93c9e3860726e7bd5d7565130c9cd198d48653cf
DING4526 <Ding13522634526@163.com>
2025-02-27 11:12:48 +0800

    Changes to be committed: modified:   main.py
    优化内容：
    1.使用“联系人”筛选器进行搜索，获得更多有效结果
    2.对同一个条目，爬取更多数据种类（查找-姓名-学号-学院-备注）
    3.对同一次查找，可以爬取多个数据项-即多个同名学生（可选）
    4.增加了灵活的容错机制（全部不筛选/全部并筛选/首条）
    5.加快了程序运行速度（搜索采用【固定时长+不定时长】，兼顾稳定性和效率）

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ import time
 
 google_chorme_address = r'C:\Program Files\Google\Chrome\ChromeDriver\chromedriver.exe'
 feishu_url = 'https://nankai.feishu.cn/next/messenger/'
-original_file_path = r"./old.xlsx"
-new_file_path = r"./new.xlsx"
+directory_path=r"./"
+# directory_path=r"D:/Desktop/"
+original_file_path = directory_path + r"old.xlsx"
+new_file_path = directory_path + r"new.xlsx"
 
 # 设置Chrome驱动
 service = Service(google_chorme_address)
@@ -44,58 +46,117 @@ def get_search_box():
     print("pressed ctrl+k")
 
     print("wait to located...")
+    li_contactor = wait.until(EC.presence_of_element_located((By.XPATH, '//div[text()="联系人"]')))
+    print("li_contactor located!")
+    li_contactor.click()
+
+    print("wait to located...")
     search_box = wait.until(EC.presence_of_element_located(
         (By.XPATH, '//*[contains(@class, "zone-container editor-kit-container notranslate chrome window chrome88")]')))
     print("Search box located!")
+    time.sleep(1)
     return search_box
 
 
-def search_info(box, name_list, record_list, delay_sec=3):
+def search_info(box, name_list, record_list, get_first, basic_filter, delay_sec=1):
+    # get_first: TRUE-只记录首个结果 / FALSE-记录查找到的所有结果
+    # basic_filter: (get_first=FALSE 时有效) TRUE-保证名字完全匹配，且排除“未激活”或“暂停使用”的用户 / FALSE-不筛选
+    # delay_sec: 将搜索框中的内容删除后，查找列表消失所用的最大时间（只需保证时间段结束后不会把上一个查找结果录入，无需保证在这个时间段内就完成下一个查找）
+
+    wait = WebDriverWait(driver, 10)
+
     for student in name_list:
         if pd.isna(student):
             record_list.append(('', '', ''))
             continue
 
-        box.click()
-        box.send_keys(Keys.CONTROL + "a")
-        box.send_keys(Keys.BACKSPACE)
         box.send_keys(student)
         print(f"Entered: {student}")
 
         time.sleep(delay_sec)
-        try:
-            stu_id = driver.find_element(By.XPATH,
-                                         '//*[contains(@class, "common-items-wrapper smart-search-tab")]/div[1]/div[2]/div[2]/div/div[1]/span')
-            stu_college = driver.find_element(By.XPATH,
-                                              '//*[contains(@class, "common-items-wrapper smart-search-tab")]/div[1]/div[2]/div[2]/div/div[2]/div/span')
-            record_tuple = (student, stu_id.text, stu_college.text)
-            record_list.append(record_tuple)
-            print(record_tuple, "done")
-        except Exception as e:
-            print(f"Error occurred for student {student}: {e}")
-            record_list.append((student, '', ''))
+
+        if get_first:
+            index = 1
+            info_wrapper = f'//*[contains(@class, "common-items-wrapper other-search-tab")]/div[{index}]/div[2]/div[2]/div'
+            try:
+                stu_id = wait.until(EC.presence_of_element_located((By.XPATH,
+                                             info_wrapper + '/div[1]/span'))).text
+                # 综合查询：id //*[contains(@class, "common-items-wrapper smart-search-tab")]/div[1]/div[2]/div[2]/div/div[1]/span
+                stu_college = wait.until(EC.presence_of_element_located((By.XPATH,
+                                                  info_wrapper + '/div[2]/div/span'))).text
+                # 综合查询：college //*[contains(@class, "common-items-wrapper smart-search-tab")]/div[1]/div[2]/div[2]/div/div[2]/div/span
+                record_tuple = (student, stu_id, stu_college)
+                record_list.append(record_tuple)
+                print(record_tuple, "done")
+            except Exception as e:
+                print(f"Error occurred for student {student}: {e}")
+                record_list.append((student, 'ERROR', 'ERROR'))
+
+        else:
+            div_wrapper = wait.until(EC.presence_of_element_located(
+                (By.XPATH, '//*[contains(@class, "common-items-wrapper other-search-tab")]')))
+            div_count = len(div_wrapper.find_elements(By.XPATH, './div'))
+            print("div_count =",div_count)
+
+            for index in range(1, div_count + 1):
+                info_wrapper = f'//*[contains(@class, "common-items-wrapper other-search-tab")]/div[{index}]/div[2]'
+                try:
+
+                    # [class]user-chatter-info-name
+                    stu_name = wait.until(EC.presence_of_element_located((By.XPATH,
+                                                     info_wrapper + '/div[1]/div[1]/div[1]'))).text.strip()
+                    stu_note = wait.until(EC.presence_of_element_located((By.XPATH,
+                                                     info_wrapper + '/div[1]/div[1]/div[2]'))).text
+                    if basic_filter:
+                        if index > 1 and not stu_name == student or stu_note == "未激活" or stu_note == "暂停使用":
+                            continue
+
+                    try:
+                        stu_id = wait.until(EC.presence_of_element_located((By.XPATH,
+                                                     info_wrapper + '/div[2]/div/div[1]/span'))).text
+                        stu_college = wait.until(EC.presence_of_element_located((By.XPATH,
+                                                          info_wrapper + '/div[2]/div/div[2]/div/span'))).text
+                        if index==1:
+                            record_tuple = (student, stu_name, stu_id, stu_college, stu_note)
+                        else:
+                            record_tuple = ('', stu_name, stu_id, stu_college, stu_note)
+                        record_list.append(record_tuple)
+                        print(record_tuple, "done")
+                    except Exception as e:
+                        print(f"Error occurred for student {student}: {e}")
+                        record_list.append((student, 'ERROR', 'ERROR', 'ERROR', 'ERROR'))
+                    # time.sleep(0.5)
+
+                except Exception:
+                    # 如果没有找到相关元素，就跳过当前用户卡片
+                    print("Skipping user card.")
+                    continue
+
+        box.send_keys(Keys.CONTROL + "a")
+        box.send_keys(Keys.BACKSPACE)
 
 
-def auto_complete_stu_info():
+def auto_complete_stu_info(get_first=False, basic_filter=True):
     data_raw = pd.read_excel(original_file_path)
     name_list = data_raw['姓名'].tolist()
-    columns = ['姓名', '学号', '学院']
+    if not get_first:
+        columns = ['查找', '姓名', '学号', '学院', '备注']
+    else:
+        columns = ['姓名', '学号', '学院']
     result_list = []
 
     visit_feishu(feishu_url)
     scan_to_login()
     search_box = get_search_box()
-    search_info(search_box, name_list, result_list)
+    search_info(search_box, name_list, result_list, get_first,basic_filter)
 
     result = pd.DataFrame(result_list, columns=columns)
     result.to_excel(new_file_path, index=False)
-    print("Total done!!!")
-
-    driver.quit()
 
 
 try:
-    auto_complete_stu_info()
+    auto_complete_stu_info(get_first=False, basic_filter=False)
+    print("Data entry has been completed!")
 except Exception as e:
     print(f"Error occurred: {e}")
 finally:
